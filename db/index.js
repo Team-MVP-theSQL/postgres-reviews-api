32bffdda1cf2d8075a18f66db329195e2b92f4bf
Giovanni Flores <geflores5@gmail.com>
2020-12-14 16:36:20 -0800

    Add pool object to db

--- db/index.js
+++ db/index.js
@@ -1,10 +1,16 @@
 const { Sequelize } = require('sequelize');
 
 const db = new Sequelize('postgresreviews', 'ec2-user', '1234', {
-  host: '18.222.175.144',
+  host: '3.17.138.203',
   port: 5432,
   dialect: 'postgres',
   'logging': false,
+  pool: {
+    max: 10,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 });
 
 db.authenticate()
